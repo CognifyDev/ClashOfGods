8310f72d3b289e1087814d632985f7ede874b114
hinhinarrrrrr <eeeeebbb10@gmail.com>
2023-07-08 12:50:56 +0800

    忘了

--- COG/States/GameStates.cs
+++ COG/States/GameStates.cs
@@ -9,5 +9,5 @@ public class GameStates
     public static bool IsInTask => InGame && !MeetingHud.Instance;
     public static bool IsMeeting => InGame && MeetingHud.Instance;
     public static bool IsVoting => IsMeeting && MeetingHud.Instance.state is MeetingHud.VoteStates.Voted or MeetingHud.VoteStates.NotVoted;
-    
+
 }
