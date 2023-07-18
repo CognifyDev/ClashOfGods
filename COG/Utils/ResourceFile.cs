abf7ef26d439e6d0527e903c67c379b46e766640
commandf1 <mkhjmy@qq.com>
2023-07-18 14:44:04 +0800

    YamlConfig完成 & 更新README & 完善其他功能

---
+++ COG/Utils/ResourceFile.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace COG.Utils;
+
+public class ResourceFile
+{
+    public string Path { get; }
+    
+    public ResourceFile(string path)
+    {
+        Path = path;
+    }
+    
+    public string GetResourcesTxt()
+    {
+        var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(Path);
+        Debug.Assert(stream != null, nameof(stream) + " != null");
+        stream.Position = 0;
+        using StreamReader reader = new(stream, Encoding.UTF8);
+        return reader.ReadToEnd();
+    }
+}
---
+++ COG/Utils/Yaml.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.RepresentationModel;
+
+namespace COG.Utils;
+
+public class Yaml
+{
+    public string Text { get; }
+    public YamlStream YamlStream { get; }
+
+    private Yaml(string text)
+    {
+        Text = text;
+
+        var input = new StringReader(Text);
+        var yamlStream = new YamlStream();
+        yamlStream.Load(input);
+
+        YamlStream = yamlStream;
+    }
+
+    public int? GetInt(string location)
+    {
+        var str = GetString(location);
+        if (str == null) return null;
+        if (int.TryParse(str, out int result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+    
+    public List<string> GetStringList(string location)
+    {
+        var locations = location.Contains('.') ? location.Split(".") : new[] { location };
+        var rootNode = (YamlMappingNode)YamlStream.Documents[0].RootNode;
+
+        if (locations.Length < 1) return new List<string>();
+
+        YamlNode? valueNode = rootNode;
+        foreach (var loc in locations)
+        {
+            try
+            {
+                if (valueNode is YamlMappingNode mappingNode)
+                {
+                    var keyNode = new YamlScalarNode(loc);
+                    if (mappingNode.Children.TryGetValue(keyNode, out valueNode))
+                    {
+                        // 继续向下查找
+                        continue;
+                    }
+                }
+
+                // 如果找不到对应的键或节点不是一个映射节点，则返回空列表
+                return new List<string>();
+            }
+            catch (KeyNotFoundException)
+            {
+                return new List<string>();
+            }
+        }
+
+        // 如果值节点是一个列表节点，则将列表中的值添加到结果列表中
+        if (valueNode is YamlSequenceNode sequenceNode)
+        {
+            var result = new List<string>();
+            foreach (var item in sequenceNode.Children)
+            {
+                if (item is YamlScalarNode { Value: not null } scalarNode) result.Add(scalarNode.Value);
+            }
+            return result;
+        }
+
+        // 如果值节点不是列表节点，则返回空列表
+        return new List<string>();
+    }
+
+    public byte? GetByte(string location)
+    {
+        var str = GetString(location);
+        if (str == null) return null;
+        if (byte.TryParse(str, out byte result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+    
+    public string? GetString(string location)
+    {
+        var locations = location.Contains('.') ? location.Split(".") : new[] { location };
+        var rootNode = (YamlMappingNode)YamlStream.Documents[0].RootNode;
+
+        if (locations.Length < 1) return null;
+
+        YamlNode? valueNode = rootNode;
+        foreach (var loc in locations)
+        {
+            try
+            {
+                if (valueNode is YamlMappingNode mappingNode)
+                {
+                    var keyNode = new YamlScalarNode(loc);
+                    if (mappingNode.Children.TryGetValue(keyNode, out valueNode))
+                    {
+                        // 继续向下查找
+                        continue;
+                    }
+                }
+
+                // 如果找不到对应的键或节点不是一个映射节点，则返回 null
+                return null;
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+        }
+        
+        return $"{valueNode}";
+    }
+
+    public static Yaml LoadFromString(string text)
+    {
+        return new Yaml(text);
+    }
+    
+    public static Yaml LoadFromFile(string path)
+    {
+        return LoadFromString(File.ReadAllText(path));
+    }
+}
