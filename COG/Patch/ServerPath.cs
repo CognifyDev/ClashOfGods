2346ce549e07d3ac4c4c2f0ec2d94543b35f212a
Huier-Huang <2133404320@qq.com>
2023-07-19 15:35:20 +0800

    添加常用公共私服 & 添加nuget.config & 修改项目文件 (#26)
    * 自动添加常用公共私服
    
    * 添加nuget.config
    
    * 修改项目文件并更新nuget包版本
    
    ---------
    
    Co-authored-by: MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>

---
+++ COG/Patch/ServerPath.cs
@@ -0,0 +1,27 @@
+﻿namespace COG.Patch;
+
+public static class ServerPath
+{
+    [HarmonyPatch(typeof(ServerManager), nameof(ServerManager.LoadServers))]
+    [HarmonyPostfix]
+    static void AddServer(ServerManager __instance)
+    {
+        IRegionInfo[] regionInfos = new IRegionInfo[]
+        {
+            CreateRegionInfo("au-sh.pafyx.top", "梦服上海(新)", 22000),
+            CreateRegionInfo("au-as.duikbo.at", "Modded Asia (MAS)", 443, true),
+            CreateRegionInfo("www.aumods.xyz", "Modded NA (MNA)", 443, true),
+            CreateRegionInfo("au-eu.duikbo.at", "Modded EU (MEU)", 443, true)
+        };
+        
+        regionInfos.Do(__instance.AddOrUpdateRegion);
+    }
+
+    static IRegionInfo CreateRegionInfo(string name, string ip, ushort port, bool isHttps = false)
+    {
+        var serverIp = isHttps ? "https://" : "http://" + ip;
+        var serverInfo = new ServerInfo(name, serverIp, port, false);
+        ServerInfo[] ServerInfo = { serverInfo };
+        return new StaticHttpRegionInfo(name, StringNames.NoTranslation, ip, ServerInfo).Cast<IRegionInfo>();
+    }
+}
