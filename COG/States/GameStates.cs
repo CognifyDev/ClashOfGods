e646c0df2d6534b6734131e6ab795f37257cb2eb
commandf1 <mkhjmy@qq.com>
2024-02-15 14:08:35 +0800

    略微修改代码

--- COG/States/GameStates.cs
+++ COG/States/GameStates.cs
@@ -6,9 +6,8 @@ public static class GameStates
 {
     public static bool InGame { get; internal set; }
     public static bool IsLobby => AmongUsClient.Instance.GameState == InnerNetClient.GameStates.Joined;
-    public static bool IsInTask => InGame && !MeetingHud.Instance;
+    public static bool InTask => InGame && !MeetingHud.Instance;
     public static bool IsMeeting => InGame && MeetingHud.Instance;
-
     public static bool IsVoting => IsMeeting &&
                                    MeetingHud.Instance.state is MeetingHud.VoteStates.Voted
                                        or MeetingHud.VoteStates.NotVoted;
