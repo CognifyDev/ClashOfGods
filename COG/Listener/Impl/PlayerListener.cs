a22cb019cd401c6978871b1444084a4dfe00f3f7
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2024-06-27 12:24:12 +0800

    尝试修复进入房间进程无响应

--- COG/Listener/Impl/PlayerListener.cs
+++ COG/Listener/Impl/PlayerListener.cs
@@ -1,23 +1,37 @@
+using Il2CppSystem.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using COG.Listener.Event.Impl.AuClient;
 using COG.UI.CustomOption;
 using COG.Utils;
+using Il2CppSystem;
+using UnityEngine;
 
 namespace COG.Listener.Impl;
 
 public class PlayerListener : IListener
 {
     [EventHandler(EventHandlerType.Postfix)]
-    public void OnCreatePlayer(AmongUsClientCreatePlayerEvent @event)
+    public void OnJoinPlayer(AmongUsClientJoinEvent @event)
     {
-        var thread = new Thread(() =>
+        var instance = @event.AmongUsClient;
+
+        List<IEnumerator> list = new()
         {
-            Thread.Sleep(500);
+            Effects.ActionAfterDelay(0.5f, (Action)ShareCurrentOptions)
+        };
+
+        void ShareCurrentOptions()
+        {
+            Main.Logger.LogInfo($"Coroutine {nameof(ShareCurrentOptions)} has started.");
             var target = PlayerUtils.GetPlayerById(@event.ClientData.Character.PlayerId);
-            if (target == null || target.IsSamePlayer(PlayerControl.LocalPlayer)) return;
-            Main.Logger.LogInfo("Sent options info to " + target.Data.PlayerName);
-            CustomOption.ShareConfigs(target);
-        });
-        thread.Start();
+            if (!(target == null || target.IsSamePlayer(PlayerControl.LocalPlayer)))
+            {
+                Main.Logger.LogInfo("Sent options info to " + target.Data.PlayerName);
+                CustomOption.ShareConfigs(target);
+            }
+        }
+
+        instance.StartCoroutine(Effects.Sequence(list.ToArray()));
     }
 }
