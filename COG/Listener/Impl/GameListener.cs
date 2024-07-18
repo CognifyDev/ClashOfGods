b110837de992214a2e7154368661b709c2f67376
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2024-07-18 19:52:21 +0800

    Fix Vanilla Bugs

--- COG/Listener/Impl/GameListener.cs
+++ COG/Listener/Impl/GameListener.cs
@@ -83,6 +83,8 @@ public class GameListener : IListener
             
             if (mainOption.RulesPreset != RulesPresets.Custom) 
                 mainOption.RulesPreset = RulesPresets.Custom;
+
+
         }
 
         if (player.PlayerId == PlayerControl.LocalPlayer.PlayerId)
@@ -214,8 +216,6 @@ public class GameListener : IListener
         roleList.AddRange(GameUtils.PlayerRoleData.SelectMany(pr => pr.SubRoles));
 
         foreach (var availableRole in roleList) availableRole.AfterSharingRoles();
-
-        LobbyBehaviour.Instance?.Despawn(); // Help innersloth solve a bug lol
     }
 
     [EventHandler(EventHandlerType.Postfix)]
---
+++ COG/Listener/Impl/HelpInnerslothFixVanillaBugsListener.cs
@@ -0,0 +1,55 @@
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+using COG.Game.CustomWinner;
+using COG.Listener.Event.Impl.Player;
+using COG.Listener.Event.Impl.RManager;
+using COG.Utils;
+using System.Collections;
+using UnityEngine;
+
+namespace COG.Listener.Impl;
+
+public class VanillaBugFixListener : IListener
+{
+    public void OnSelectRole(RoleManagerSelectRolesEvent _)
+    {
+        var lobby = LobbyBehaviour.Instance;
+        if (lobby)
+        {
+            Main.Logger.LogWarning("Lobby room still exists in this room, trying to fix...");
+            lobby.Despawn();
+        }
+    }
+
+    public void OnEjectionEnd(ExileController controller)
+    {
+        controller.StartCoroutine(CoFixBlackout().WrapToIl2Cpp());
+    }
+
+    public void OnAirshipEjectionEnd(PlayerExileEndOnAirshipEvent @event) => OnEjectionEnd(@event.Controller);
+    public void OnOtherMapEjectionEnd(PlayerExileEndEvent @event) => OnEjectionEnd(@event.ExileController);
+
+    public IEnumerator CoFixBlackout()
+    {
+        yield return new WaitForSeconds(1f);
+
+        var hud = HudManager.Instance;
+        var fullScr = hud.FullScreen.gameObject;
+        if (!fullScr.active) yield break;
+
+        Main.Logger.LogInfo("After-meeting blackout bug has occured. Trying to fix...");
+        var auClient = AmongUsClient.Instance;
+        var mapId = (MapNames)(auClient.NetworkMode == NetworkModes.FreePlay ? auClient.TutorialMapId : GameUtils.GetGameOptions().MapId);
+        
+        if (mapId is MapNames.Airship)
+        {
+            Main.Logger.LogError("The game will end now because you're playing in Airship and we can't fix blackout bug in Airship.");
+            CustomWinnerManager.EndGame(PlayerControl.AllPlayerControls.ToArray(), "Force End Due to Bug", Palette.White);
+        }
+        else
+        {
+            fullScr.SetActive(false);
+            Camera.main.GetComponent<FollowerCamera>().Locked = false;
+            Main.Logger.LogInfo("Fixed successfully!");
+        }
+    }
+}
