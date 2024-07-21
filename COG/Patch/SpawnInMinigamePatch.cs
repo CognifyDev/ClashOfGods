bea03c13845d1bdc12cf33990b33d1474884bdbe
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2024-07-21 20:47:49 +0800

    修不完的小bug

---
+++ COG/Patch/SpawnInMinigamePatch.cs
@@ -0,0 +1,15 @@
+using COG.Listener.Impl;
+using SpawnInMinigame_WaitForFinish = SpawnInMinigame._WaitForFinish_d__13;
+
+namespace COG.Patch;
+
+[HarmonyPatch(typeof(SpawnInMinigame_WaitForFinish), nameof(SpawnInMinigame_WaitForFinish.MoveNext))]
+internal class SpawnInMinigameBlackoutFixPatch
+{
+    public static void Postfix(bool __result)
+    {
+        var hud = HudManager.Instance;
+        if(VanillaBugFixListener.OccuredBlackoutOnAirship&&!__result)
+            hud.StartCoroutine(hud.CoFadeFullScreen(new(0, 0, 0, 1), new(0, 0, 0, 0)));
+    }
+}
