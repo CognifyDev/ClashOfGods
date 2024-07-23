7c8170c9bf3a886b1f01268e4cc29f2a33ef27c2
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2024-07-23 21:26:20 +0800

    Custom Option Update

--- COG/Listener/Impl/RpcListener.cs
+++ COG/Listener/Impl/RpcListener.cs
@@ -47,7 +47,7 @@ public class RpcListener : IListener
                             if (option == null) continue;
                             if (option.ID != id) continue;
                             Main.Logger.LogInfo(
-                                $"Changed {option.RealNameDelegate()}({option.ID})'s selection to {selection}(before: {option.Selection})");
+                                $"Changed {option.RealName()}({option.ID})'s selection to {selection}(before: {option.Selection})");
                             option.Selection = selection;
                             CustomOption.Options[i] = option;
                         }
