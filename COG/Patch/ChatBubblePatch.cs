f47d606640b52a6aca692c08a6b557ee92936dad
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2024-08-02 14:01:08 +0800

    fix

--- COG/Patch/ChatBubblePatch.cs
+++ COG/Patch/ChatBubblePatch.cs
@@ -5,14 +5,9 @@ namespace COG.Patch;
 [HarmonyPatch(typeof(ChatBubble), nameof(ChatBubble.SetText))]
 internal static class ChatBubblePatch
 {
-    private static bool Prefix(ChatBubble __instance)
+    private static void Postfix(ChatBubble __instance)
     {
         if (__instance.NameText.text == LanguageConfig.Instance.SystemMessage)
-        {
             __instance.SetLeft();
-            return false;
-        }
-
-        return true;
     }
 }
