2f988972308ee8d1a1f57e20b7dc6f2924b1f1d3
JieGeLovesDengDuaLang <echouyeshouxianbei@163.com>
2025-08-06 16:53:42 +0800

    11

--- COG/Listener/Impl/CommandListener.cs
+++ COG/Listener/Impl/CommandListener.cs
@@ -25,31 +25,14 @@ public class CommandListener : IListener
         return !result.OnExecute(PlayerControl.LocalPlayer, AsCommandStringArray(text));
     }
 
-    // Handling other players' chat command and executing aren't necessary
-
-    //[EventHandler(EventHandlerType.Postfix)]
-    //public void OnPlayerChat(PlayerChatEvent @event)
-    //{
-    //    var text = @event.Text;
-    //    var player = @event.Player;
-    //    if (!text.ToLower().StartsWith("/")) return;
-
-    //    foreach (var command in CommandManager.GetManager().GetCommands().Where(command => !command.HostOnly)
-    //                 .Where(command => text.Split(" ")[0].ToLower().Equals("/" + command.Name.ToLower()) ||
-    //                                   ContainAliases(text, command)))
-    //        command.OnExecute(player, AsCommandStringArray(text));
-    //}
-
     private string[] AsCommandStringArray(string text)
     {
         // /command <arg1> <arg3> <arg2>...
-        var args = text.Split(' ');
-        var toReturn = new string[args.Length - 1];
-        Array.Copy(args, 1, toReturn, 0, toReturn.Length);
-        return toReturn;
+        var args = ;
+        return text.Split(' ').Skip(1).ToArray();
     }
 
-    private bool ContainAliases(string text, Command.CommandBase command)
+    private bool ContainAliases(string text, CommandBase command)
     {
         foreach (var alias in command.Aliases)
             if (text.Split(" ")[0].ToLower().Equals("/" + alias.ToLower()))
