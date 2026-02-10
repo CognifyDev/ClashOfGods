9cac96be86a47622cd76843ec70195ec65715919
linmeideli <2558527373@qq.com>
2026-02-10 12:23:37 +0800

    计划做一个UI

--- COG/UI/Hud/InnerHud/HudAssets.cs
+++ COG/UI/Hud/InnerHud/HudAssets.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using COG.Utils;
+using UnityEngine;
 
 namespace COG.UI.Hud.InnerHud
 {
-    internal class HudAssets
+    public static class HudAssets
     {
+        public static Sprite BackgroundInner {  get; set; }
+        public static Sprite BackgroundOuter { get; set; }
+        public static Sprite CancelButton {  get; set; }
+        public static Sprite MarkButton { get; set; }
+        public static Sprite Highlight { get; set; }
+
+        public static void LoadUiSprites()
+        {
+            var loader = DividedSpriteLoader.FromResource("", 100f, 2, 1, false);
+
+            Sprite firstElement = loader.GetSprite(0);
+            Sprite secondElement = loader.GetSprite(1);
+        }
     }
 }
--- COG/UI/Hud/InnerHud/HudText.cs
+++ COG/UI/Hud/InnerHud/HudText.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using COG.UI.Hud.InnerHud.Core;
 
 namespace COG.UI.Hud.InnerHud
 {
-    internal class HudText
+    public class HudText : HudBase
     {
+        public HudText(string text)
+        {
+
+        }
     }
 }
